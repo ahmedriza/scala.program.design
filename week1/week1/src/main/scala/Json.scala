3466e09f0f5bef77c2c6859d40cd690d3b20b169
Ahmed Riza <dr.ahmed_riza@yahoo.co.uk>
2018-05-06 20:37:46 +0100

    week1

--- week1/week1/src/main/scala/Json.scala
+++ week1/week1/src/main/scala/Json.scala
@@ -2,12 +2,12 @@
 
 abstract class Json
 
-case class JSeq (elems: List[Json])           extends Json
-case class JObj (bindings: Map[String, Json]) extends Json
-case class JNum (num: Double)                 extends Json
-case class JStr (str: String)                 extends Json
-case class JBool (b: Boolean)                 extends Json
-case object JNull                             extends Json
+case class JSeq(elems: List[Json]) extends Json
+case class JObj(bindings: Map[String, Json]) extends Json
+case class JNum(num: Double) extends Json
+case class JStr(str: String) extends Json
+case class JBool(b: Boolean) extends Json
+case object JNull extends Json
 
 class Animal
 
@@ -20,38 +20,57 @@ object Json {
         "firstName" -> JStr("John"),
         "lastName" -> JStr("Smith"),
         "address" -> JObj(
-          Map("streeAddress" -> JStr("21 2nd Street"),
+          Map(
+            "streeAddress" -> JStr("21 2nd Street"),
             "state" -> JStr("NY"),
-            "postalCode" -> JNum(10021)
-          )
-        ),
+            "postalCode" -> JNum(10021))),
         "phoneNumbers" -> JSeq(List(
           JObj(Map("type" -> JStr("home"), "number" -> JStr("212 555-1234"))),
-          JObj(Map("type" -> JStr("fax"), "number" -> JStr("646 555-4567")))
-        ))
-      )
-    )
+          JObj(Map("type" -> JStr("fax"), "number" -> JStr("646 555-4567")))))))
 
     val str = show(JSeq(List(
       JObj(Map("type" -> JStr("home"), "number" -> JStr("212 555-1234"))),
-      JObj(Map("type" -> JStr("fax"), "number" -> JStr("646 555-4567")))
-    )))
+      JObj(Map("type" -> JStr("fax"), "number" -> JStr("646 555-4567"))))))
 
     println(show(json))
+    caseFunctions()
   }
+
+  type JBinding = (String, Json)
 
   def show(json: Json): String = json match {
     case JSeq(elems) =>
       "[" + elems.map(show).mkString(", ") + "]"
     case JObj(bindings) =>
-      "{" +
-        bindings.map { case (key, value) => '\"' + key + "\": " + show(value) }.mkString(", ")  +
-        "}"
+      val func: JBinding => String = { case (key: String, value: Json) => key + ": " + show(value) }
+      val assocs = bindings.map(func)
+      "{" + assocs.mkString(", ") + "}"
     case JNum(num) => num.toString
     case JStr(str) => '\"' + str + '\"'
     case JBool(b) => b.toString
     case JNull => "null"
   }
 
+  def caseFunctions(): Unit = {
+
+    val f: PartialFunction[String, String] = {
+      case "ping" => "pong"
+    }
+
+    val g: PartialFunction[List[Int], String] = {
+      case Nil => "one"
+      case x :: rest =>
+        rest match {
+          case Nil => "two"
+        }
+    }
+
+    println(f("ping"))
+
+    println(f.isDefinedAt("ping"))
+    println(f.isDefinedAt("pong"))
+
+    println(g.isDefinedAt(List(1,2,3)))
+  }
 
 }
