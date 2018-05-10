8749efaa060008ffee0a20d1df09f991e45b7abc
Ahmed Riza <dr.ahmed_riza@yahoo.co.uk>
2018-05-10 21:34:03 +0100

    week2

--- week2/week2/src/main/scala/LazyEvaluation.scala
+++ week2/week2/src/main/scala/LazyEvaluation.scala
@@ -29,6 +29,55 @@
 //     lazy val tail = tl
 //
 
+trait LazyStream[+A] {
+  def isEmpty: Boolean
+  def head: A
+  def tail: LazyStream[A]
+
+  override def toString: String = "LazyStream(" + head + ", ?)"
+
+  def apply(n: Int): A =
+    if (n == 0) head
+    else tail.apply(n - 1)
+
+  // Other methods are implemented analogously to their List counterparts
+  def filter(p: A => Boolean): LazyStream[A] =
+    if (isEmpty) this
+    // Note that in the line below, the recursive call
+    // to filter happens as the 2nd parameter to cons() which is
+    // a by-name parameter, hence the tail won't be evaluated until
+    // someone actually asks for it.
+    else if (p(head)) LazyStream.cons(head, tail.filter(p))
+    else tail.filter(p)
+}
+
+object LazyStream {
+
+  // Note that the tl parameter is by-name
+  // Contrast this with the Cons class for Lists where the tail parameter
+  // is a normal call-by-value parameter
+  // That is the only thing that matters between Lists and Streams.
+
+  def cons[T](hd: T, tl: => LazyStream[T]): LazyStream[T] = new LazyStream[T] {
+
+    override def isEmpty: Boolean = false
+
+    override def head: T = hd
+
+    lazy val tail: LazyStream[T] = tl
+  }
+
+  val empty: LazyStream[Nothing] = new LazyStream[Nothing] {
+
+    override def isEmpty: Boolean = true
+
+    override def head = throw new NoSuchElementException("empty.head")
+
+    override def tail = throw new NoSuchElementException("empty.tail")
+  }
+}
+
+
 object LazyEvaluation {
 
 
@@ -42,21 +91,30 @@ object LazyEvaluation {
       z + y + x + z + y + x
     }
 
-    expr
-
     // What gets printed as a side effect of evaluating expr?
     // "xzyz"
+
+
+    def isPrime(n: Int): Boolean = (2 until n).forall(x => n % x != 0)
+
+    def streamRange(lo: Int, hi: Int): LazyStream[Int] = {
+      if (lo >= hi) LazyStream.empty
+      else LazyStream.cons(lo, streamRange(lo + 1, hi))
+    }
+
+    println(streamRange(1, 100))
 
     // Seeing lazy evaluation in action:
     //
     // Let's use the substitution model to figure out what happens to:
     //
-    // streamRange(1000, 10000) filter isPrime) apply 1
+    val result = (streamRange(1000, 10000) filter isPrime) apply 1
+    println(result)
+
     //
     // if (1000 > 10000) empty
     // else cons(1000, streamRange(1000 + 1, 10000))
     // .filter(isPrime).apply(1)
-    // 
-
+    //
   }
 }
